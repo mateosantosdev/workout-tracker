2062f71c6797a7b0075a9dc36a6d7b122fa1f827
Enrique Paulino <enrique-paulino@outlook.com>
2025-05-17 21:20:21 +0100

    Update entrypoint to open the right file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Run database initialisation first
-python init_app.py
+python -m app.init_app
 
 # Then start the app with waitress
 exec waitress-serve --listen=0.0.0.0:${PORT:-5000} run:app
